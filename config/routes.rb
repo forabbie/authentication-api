3876cce0b9216a0a7ed63d49bcc7eae94442e52a
abbie <supermercadoabbie@gmail.com>
2023-09-19 23:45:28 +0800

    -- feat: setup the auth with devise + jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # devise_for :users
+  # devise_scope :user do
+  #   post '/users/sign_up', to: 'devise/registrations#create'
+  # end
+  devise_for :users, path: '', path_names: {
+    sign_in: '/auth/login',
+    sign_out: '/auth/logout',
+    registration: '/auth/signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
